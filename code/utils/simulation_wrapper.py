11c6a19063187fdc13b75a341325b32f808e468c
andererka <k.anderer@t-online.de>
2021-11-04 15:28:54 +0100

    expand

--- code/utils/simulation_wrapper.py
+++ code/utils/simulation_wrapper.py
@@ -12,9 +12,76 @@ def simulation_wrapper(params):   #input possibly array of 1 or more params
 
     Summarizes the output of the HH simulator and converts it to `torch.Tensor`.
     """
+    net = set_network_2_params(params)
+    #net = set_network_4_params(params)
+    
+    window_len, scaling_factor = 30, 3000
+    #net._params['t_evdist_1'] = params[0]
+    #net._params['sigma_t_evdist_1'] = params[1]
+    #net._params['t_evprox_2'] = params[1]
+    #net._params['sigma_t_evprox_2'] = params[3]
+    #net._params['L2Pyr_soma_diam'] = params[4]   # 23.4
+    #net._params['L5Pyr_apical1_diam'] = params[5]     # 7.48
+
+    ##simulates 8 trials at a time like this
+    dpls = simulate_dipole(net, tstop=170., n_trials=1)
+    for dpl in dpls:
+        obs = dpl.smooth(window_len).scale(scaling_factor).data['agg']
+
+    #left out summary statistics for a start
+    sum_stats = calculate_summary_stats(torch.from_numpy(obs))
+    return sum_stats
+
+
+from random import randrange
+
+def event_seed():
+    seed = randrange(200)
+    return seed
+
+
+def set_network_default(params=None):
+
     net = jones_2009_model()
+    weights_ampa_d1 = {'L2_basket': 0.006562, 'L2_pyramidal': .000007,
+                   'L5_pyramidal': 0.142300}
+    weights_nmda_d1 = {'L2_basket': 0.019482, 'L2_pyramidal': 0.004317,
+                   'L5_pyramidal': 0.080074}
+    synaptic_delays_d1 = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
+                      'L5_pyramidal': 0.1}
+    net.add_evoked_drive(
+    'evdist1', mu=63.53, sigma=3.85, numspikes=1, weights_ampa=weights_ampa_d1,
+    weights_nmda=weights_nmda_d1, location='distal',
+    synaptic_delays=synaptic_delays_d1, event_seed=event_seed())
 
 
+
+
+    weights_ampa_p1 = {'L2_basket': 0.08831, 'L2_pyramidal': 0.01525,
+                   'L5_basket': 0.19934, 'L5_pyramidal': 0.00865}
+    synaptic_delays_prox = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
+                        'L5_basket': 1., 'L5_pyramidal': 1.}
+
+    # all NMDA weights are zero; pass None explicitly
+    net.add_evoked_drive(
+    'evprox1', mu=26.61, sigma=2.47, numspikes=1, weights_ampa=weights_ampa_p1,
+    weights_nmda=None, location='proximal',
+    synaptic_delays=synaptic_delays_prox, event_seed=event_seed())
+
+    # Second proximal evoked drive. NB: only AMPA weights differ from first
+    weights_ampa_p2 = {'L2_basket': 0.000003, 'L2_pyramidal': 1.438840,
+                   'L5_basket': 0.008958, 'L5_pyramidal': 0.684013}
+    # all NMDA weights are zero; omit weights_nmda (defaults to None)
+    net.add_evoked_drive(
+    'evprox2', mu=137.12, sigma=8.33, numspikes=1,
+    weights_ampa=weights_ampa_p2, location='proximal',
+    synaptic_delays=synaptic_delays_prox, event_seed=event_seed())
+
+    return net
+
+def set_network_2_params(params=None):
+
+    net = jones_2009_model()
     weights_ampa_d1 = {'L2_basket': 0.006562, 'L2_pyramidal': .000007,
                    'L5_pyramidal': 0.142300}
     weights_nmda_d1 = {'L2_basket': 0.019482, 'L2_pyramidal': 0.004317,
@@ -48,27 +115,45 @@ def simulation_wrapper(params):   #input possibly array of 1 or more params
     'evprox2', mu=params[1], sigma=8.33, numspikes=1,
     weights_ampa=weights_ampa_p2, location='proximal',
     synaptic_delays=synaptic_delays_prox, event_seed=event_seed())
-    
-    window_len, scaling_factor = 30, 3000
-    #net._params['t_evdist_1'] = params[0]
-    #net._params['sigma_t_evdist_1'] = params[1]
-    #net._params['t_evprox_2'] = params[1]
-    #net._params['sigma_t_evprox_2'] = params[3]
-    #net._params['L2Pyr_soma_diam'] = params[4]   # 23.4
-    #net._params['L5Pyr_apical1_diam'] = params[5]     # 7.48
 
-    ##simulates 8 trials at a time like this
-    dpls = simulate_dipole(net, tstop=170., n_trials=1)
-    for dpl in dpls:
-        obs = dpl.smooth(window_len).scale(scaling_factor).data['agg']
-
-    #left out summary statistics for a start
-    sum_stats = calculate_summary_stats(torch.from_numpy(obs))
-    return sum_stats
+    return net
 
 
-from random import randrange
+def set_network_6_params(params=None):
 
-def event_seed():
-    seed = randrange(200)
-    return seed
+    net = jones_2009_model()
+    weights_ampa_d1 = {'L2_basket': 0.006562, 'L2_pyramidal': .000007,
+                   'L5_pyramidal': 0.142300}
+    weights_nmda_d1 = {'L2_basket': 0.019482, 'L2_pyramidal': 0.004317,
+                   'L5_pyramidal': 0.080074}
+    synaptic_delays_d1 = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
+                      'L5_pyramidal': 0.1}
+    net.add_evoked_drive(
+    'evdist1', mu=params[0], sigma=params[1], numspikes=1, weights_ampa=weights_ampa_d1,
+    weights_nmda=weights_nmda_d1, location='distal',
+    synaptic_delays=synaptic_delays_d1, event_seed=event_seed())
+
+
+
+
+    weights_ampa_p1 = {'L2_basket': 0.08831, 'L2_pyramidal': 0.01525,
+                   'L5_basket': 0.19934, 'L5_pyramidal': 0.00865}
+    synaptic_delays_prox = {'L2_basket': 0.1, 'L2_pyramidal': 0.1,
+                        'L5_basket': 1., 'L5_pyramidal': 1.}
+
+    # all NMDA weights are zero; pass None explicitly
+    net.add_evoked_drive(
+    'evprox1', mu=params[2], sigma=params[3], numspikes=1, weights_ampa=weights_ampa_p1,
+    weights_nmda=None, location='proximal',
+    synaptic_delays=synaptic_delays_prox, event_seed=event_seed())
+
+    # Second proximal evoked drive. NB: only AMPA weights differ from first
+    weights_ampa_p2 = {'L2_basket': 0.000003, 'L2_pyramidal': 1.438840,
+                   'L5_basket': 0.008958, 'L5_pyramidal': 0.684013}
+    # all NMDA weights are zero; omit weights_nmda (defaults to None)
+    net.add_evoked_drive(
+    'evprox2', mu=params[4], sigma=params[5], numspikes=1,
+    weights_ampa=weights_ampa_p2, location='proximal',
+    synaptic_delays=synaptic_delays_prox, event_seed=event_seed())
+
+    return net 
