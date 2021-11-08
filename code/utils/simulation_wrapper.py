2d8e5b8684e6c97550891b9b5cdcc8f53653ab9c
andererka <k.anderer@t-online.de>
2021-11-08 11:23:29 +0100

    6 params

--- code/utils/simulation_wrapper.py
+++ code/utils/simulation_wrapper.py
@@ -10,20 +10,17 @@ def simulation_wrapper(params):   #input possibly array of 1 or more params
     """
     Returns summary statistics from conductance values in `params`.
 
+    One can outcomment the line 'net = set_network_2_params' and instead choose 'net = set_network_6_params'
+    in order to infer more than 2 parameters
+
     Summarizes the output of the HH simulator and converts it to `torch.Tensor`.
     """
-    net = set_network_2_params(params)
-    #net = set_network_4_params(params)
+    #net = set_network_2_params(params)
+    net = set_network_6_params(params)
     
     window_len, scaling_factor = 30, 3000
-    #net._params['t_evdist_1'] = params[0]
-    #net._params['sigma_t_evdist_1'] = params[1]
-    #net._params['t_evprox_2'] = params[1]
-    #net._params['sigma_t_evprox_2'] = params[3]
-    #net._params['L2Pyr_soma_diam'] = params[4]   # 23.4
-    #net._params['L5Pyr_apical1_diam'] = params[5]     # 7.48
 
-    ##simulates 8 trials at a time like this
+
     dpls = simulate_dipole(net, tstop=170., n_trials=1)
     for dpl in dpls:
         obs = dpl.smooth(window_len).scale(scaling_factor).data['agg']
