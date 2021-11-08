4893fd75ccb7b8062459535d078535618125d5a5
andererka <k.anderer@t-online.de>
2021-11-08 18:30:15 +0100

    results

--- code/script.sh
+++ code/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --ntasks=1                # Number of tasks (see below)
-#SBATCH --cpus-per-task=32         # Number of CPU cores per task
+#SBATCH --cpus-per-task=8         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-10:00            # Runtime in D-HH:MM
+#SBATCH --time=0-12:00            # Runtime in D-HH:MM
 #SBATCH --mem=50G                # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=hostname_%j.out  # File to which STDOUT will be written
 #SBATCH --error=hostname_%j.err   # File to which STDERR will be written
@@ -13,4 +13,4 @@
 scontrol show job $SLURM_JOB_ID 
 
 # insert your commands here
-python ERP_simulation_and_inference.py
+python3 ERP_simulation_and_inference.py 1000 'nsf' 8 500
