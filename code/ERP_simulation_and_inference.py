dd51d935ed5602d05776e656049c2242bdd1337f
andererka <k.anderer@t-online.de>
2021-11-15 13:31:44 +0100

    snpe_c

--- code/ERP_simulation_and_inference.py
+++ code/ERP_simulation_and_inference.py
@@ -125,7 +125,7 @@ def main(argv):
 
 
 
-    file_writer = write_to_file.WriteToFile(experiment='ERP_{}_num_params:{}_'.format(density_estimator, true_params.size(dim=0)), num_sim=number_simulations,
+    file_writer = write_to_file.WriteToFile(experiment='ERP_{}_num_params:{}_'.format(density_estimator, num_params), num_sim=number_simulations,
                     true_params=true_params, density_estimator=density_estimator, num_params=num_params)
 
 
