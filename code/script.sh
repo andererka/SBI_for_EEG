d36d221c653e8305b053b05b70d0e7bc85958da2
andererka <k.anderer@t-online.de>
2021-11-23 14:31:08 +0100

    sim with

--- code/script.sh
+++ code/script.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=64         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-01:00            # Runtime in D-HH:MM
+#SBATCH --time=0-03:00            # Runtime in D-HH:MM
 #SBATCH --mem=50G                # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=hostname_%j.out  # File to which STDOUT will be written
 #SBATCH --error=hostname_%j.err   # File to which STDERR will be written
