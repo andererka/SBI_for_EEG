a5ebfff7ea76ffd9870064c1ebd9c3f4cc6457cd
andererka <k.anderer@t-online.de>
2021-11-24 15:27:46 +0100

    sequential

--- code/sequential_inference.py
+++ code/sequential_inference.py
@@ -38,11 +38,10 @@ def main(argv):
             set the parameter values to the same values all the times)
     """
 
-    
     try:
-        file = argv[0]
+        num_sim = int(argv[0])
     except:
-        file = 'results/ERP_save_sim_nsf_num_params:3_11-23-2021_15:58:04/class'
+        num_sim = 100
     try:
         num_samples = int(argv[1])
     except:
@@ -59,46 +58,15 @@ def main(argv):
 
 
 
-    ### loading the class:
-    with open(file, 'rb') as pickle_file:
-        file_writer = pickle.load(pickle_file)
+
+    prior_min = [43.8, 89.49, 7.9]    # 't_evdist_1', 't_evprox_1', 't_evprox_2'
+
+    prior_max = [79.9, 152.96, 30]
+
+    true_params = torch.tensor([[63.53, 137.12, 18.97]]) 
+    parameter_names =  ['t_evdist_1',  't_evprox_1',  't_evprox_2']
 
 
-    ##loading theta and x from results
-    theta = lf.load_thetas(file_writer.folder)
-    x_without = lf.load_obs_without(file_writer.folder)
-    x = lf.load_obs(file_writer.folder)
-
-    num_params = theta.size(dim=1)
-    print(num_params)
-    if (num_params==6):
-        prior_min = [43.8, 3.01, 11.364, 1.276, 89.49, 5.29]   # 't_evdist_1', 'sigma_t_evdist_1', 't_evprox_1', 'sigma_t_evprox_1', 't_evprox_2', 'sigma_t_evprox_2'
-
-        prior_max = [79.9, 9.03, 26.67, 3.828, 152.96, 15.87]  
-        
-        true_params = torch.tensor([[61.89, 6.022, 19.01, 2.55, 121.23, 10.58]])   
-
-        parameter_names =  ['t_evdist_1', 'sigma_t_evdist_1', 't_evprox_1', 'sigma_t_evprox_1', 't_evprox_2', 'sigma_t_evprox_2']
-    
-
-    if (num_params==3):
-        prior_min = [43.8, 89.49, 7.9]    # 't_evdist_1', 't_evprox_1', 't_evprox_2'
-
-        prior_max = [79.9, 152.96, 30]
-
-        true_params = torch.tensor([[63.53, 137.12, 18.97]]) 
-        parameter_names =  ['t_evdist_1',  't_evprox_1',  't_evprox_2']
-
-    if (num_params==2):
-        prior_min = [43.8, 89.49] 
-        prior_max = [79.9, 152.96]
-
-        true_params = torch.tensor([[63.53, 137.12]]) 
-        parameter_names =  ['t_evdist_1',  't_evprox_1']
-
-    elif (num_params==None):
-        print('number of parameters must be defined in the arguments')
-        sys.exit()
         
 
     ###### starting with P50 parameters/summary stats:
@@ -120,7 +88,7 @@ def main(argv):
 
     posterior = inf.build_posterior(density_estimator)
 
-    _, obs_real = inference.run_only_sim(true_params[1], num_workers=num_workers)   # first output gives summary statistics, second without
+    _, obs_real = inference.run_only_sim(true_params[0][1], num_workers=num_workers)   # first output gives summary statistics, second without
 
     obs_real = calculate_summary_stats_P50(obs_real)
 
@@ -160,7 +128,7 @@ def main(argv):
 
     posterior = inf.build_posterior(density_estimator)
 
-    _, obs_real = inference.run_only_sim(true_params[0:1], num_workers=num_workers)   # first output gives summary statistics, second without
+    _, obs_real = inference.run_only_sim(true_params[0][0:1], num_workers=num_workers)   # first output gives summary statistics, second without
 
     obs_real = calculate_summary_stats_N100(obs_real)
 
@@ -187,7 +155,8 @@ def main(argv):
 
     inf = SNPE_C(prior3, density_estimator='nsf')
 
-    posterior, theta, _, x_without = inference.run_sim_inference(prior3, num_simulations=100, num_workers =num_workers, density_estimator='nsf')
+
+    posterior, theta, _, x_without = inference.run_sim_inference(prior3, num_simulations=num_sim, num_workers =num_workers, density_estimator='nsf')
 
 
 
@@ -209,7 +178,7 @@ def main(argv):
 
 
 
-    true_params = lf.load_true_params(file_writer.folder)
+
     limits = [list(tup) for tup in zip(prior_min,prior_max)]
 
     fig, axes = analysis.pairplot(samples,
@@ -220,6 +189,10 @@ def main(argv):
                             points_offdiag={'markersize': 6},
                             points_colors='r',
                             label_samples=parameter_names);
+
+    file_writer = write_to_file.WriteToFile(experiment='ERP_sequential_{}'.format(density_estimator), num_sim=num_sim,
+                    true_params=true_params, density_estimator=density_estimator, num_params=3, num_samples=num_samples)
+
 
     file_writer.save_fig(fig, figname='sequential_approach')
 
