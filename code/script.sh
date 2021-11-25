bf23d83e7cd2cd22e8eefa554ab6d8954466e555
andererka <k.anderer@t-online.de>
2021-11-25 21:03:24 +0100

    slurm result 25

--- code/script.sh
+++ code/script.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=64         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=0-10:00            # Runtime in D-HH:MM
+#SBATCH --time=0-08:00            # Runtime in D-HH:MM
 #SBATCH --mem=50G                # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=hostname_%j.out  # File to which STDOUT will be written
 #SBATCH --error=hostname_%j.err   # File to which STDERR will be written
@@ -13,5 +13,4 @@
 scontrol show job $SLURM_JOB_ID 
 
 # insert your commands he
-python3 sequential_inference.py 1000 100 1 64
-
+python sequential_inference.py 1000 1000 1 64
