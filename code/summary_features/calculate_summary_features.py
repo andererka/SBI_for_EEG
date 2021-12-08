9ac92b3de99124cca47a30eb9d8b316f4665767e
andererka <k.anderer@t-online.de>
2021-12-08 09:34:44 +0100

    size

--- code/summary_features/calculate_summary_features.py
+++ code/summary_features/calculate_summary_features.py
@@ -134,7 +134,7 @@ def calculate_summary_stats_P50(x):
 
     """
 
-    print('len of x in sum stat function:', x.size())
+    print('len of x in sum stat function:', len(x))
     time_window = 30
 
     batch_list = []
