8b5cf36bcd984f4b6f5e56357987ef4561134a40
andererka <k.anderer@t-online.de>
2021-12-25 00:21:33 +0100

    other idea for downsampling

--- code/utils/simulation_wrapper.py
+++ code/utils/simulation_wrapper.py
@@ -100,7 +100,7 @@ def simulation_wrapper_obs(params):  # input possibly array of 1 or more params
     Summarizes the output of the HH simulator and converts it to `torch.Tensor`.
     """
 
-    early_stop = 40.0
+    early_stop = 170.0
 
 
     if params.dim() > 1:
