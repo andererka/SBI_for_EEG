1d388d2a5e23d19a4303655c4f36863447b1e8c6
andererka <k.anderer@t-online.de>
2021-12-25 11:59:11 +0100

    20steps alternative calc

--- code/summary_features/calculate_summary_features.py
+++ code/summary_features/calculate_summary_features.py
@@ -236,7 +236,7 @@ def calculate_summary_stats_temporal(x):
 def calculate_summary_statistics_alternative(x, number=0):
     if (x.dim()==2):
         print(x.shape)
-        sum_stat = x[:,::10]
+        sum_stat = x[:,::20]
         print(sum_stat.shape)
     else:
         sum_stat = x[::10]
