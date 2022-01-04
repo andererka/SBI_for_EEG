44f627c64d30984403cf6636736bad3d79891b5c
Katharina Anderer <kanderer29@bg-slurmb-login1.novalocal>
2022-01-04 16:11:50 +0100

    results

--- code/efficiency_comparison_summary_statistics.py
+++ code/efficiency_comparison_summary_statistics.py
@@ -69,7 +69,7 @@ def main(argv):
     try:
         experiment_name = argv[1]
     except:
-        experiment_name = 'cnn_sum_stats_500sim_2params'
+        experiment_name = '6_sum_stats_500sim_3params'
 
     ### loading the class:
     with open('results/{}/class'.format(file), "rb") as pickle_file:
