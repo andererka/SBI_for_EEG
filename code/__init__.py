93f4164f5e81776584f7df6bedfa848937b1ec11
andererka <k.anderer@t-online.de>
2022-01-05 12:36:03 +0100

    import class

--- code/__init__.py
+++ code/__init__.py
@@ -1,4 +1,4 @@
-from data_load_writer import load_from_file, write_to_file
+from .data_load_writer import load_from_file, write_to_file
 from summary_features import calculate_summary_features
 from .utils.simulation_wrapper import simulation_wrapper_obs, simulation_wrapper_all, simulation_wrapper
 from .summary_features import calculate_summary_features
