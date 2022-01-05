10a8cdec79844b0cfc9c5b665dcb422f3f9e215d
andererka <k.anderer@t-online.de>
2022-01-05 11:53:45 +0100

    other results file

--- code/efficiency_comparison_summary_statistics.py
+++ code/efficiency_comparison_summary_statistics.py
@@ -117,10 +117,10 @@ def main(argv):
 
 
     #thetas = torch.load('thetas.pt')
-    with open('{}/thetas.pt'.format(file), "rb") as torch_file:
+    with open('{}/thetas'.format(file), "rb") as torch_file:
         thetas = torch.load(torch_file)
 
-    with open('{}/obs_without.pt'.format(file), "rb") as torch_file:
+    with open('{}/obs_without'.format(file), "rb") as torch_file:
         x_without = torch.load(torch_file)
 
     #thetas = torch.load('{}/thetas.pt'.format(file))
