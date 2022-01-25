fcff1197b1e4763853a52bdb7c393d75e5cf1560
andererka <k.anderer@t-online.de>
2022-01-25 10:18:59 +0100

    slurm cache

--- code/script.sh
+++ code/script.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=63         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=1-00:00            # Runtime in D-HH:MM
+#SBATCH --time=2-00:00            # Runtime in D-HH:MM
 #SBATCH --mem=70G                # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=hostname_%j.out  # File to which STDOUT will be written
 #SBATCH --error=hostname_%j.err   # File to which STDERR will be written
@@ -16,6 +16,7 @@ scontrol show job $SLURM_JOB_ID
 #python3 sequential_inference.py 5000 1000 64 5000_sims_3_params 0
 #python3 calc_posterior_and_sample.py results/ERP_save_sim_nsf_num_params:3_11-25-2021_21:36:41/class 1000 1 64 
 #python3 ERP_simulation_and_inference.py 500 nsf 64 7 rejection
-python3 sequential_inference_6params.py 100 100 63 100_sims_6_params_new2 1
+python3 sequential_inference_6params.py 10000 1000 63 10000_sims_6_params_times 0
 #python3 efficiency_comparison_summary_statistics.py True
-#python3 investigating_summary_features.py eval_features 
+#python3 investigating_summary_features.py eval_features
+#python3 simulate_only.py 10000 sim_10000_3_params_non_seq2 63 
