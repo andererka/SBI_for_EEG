fe37bb0aee11f80bb84567e2cc46f57a86db33a5
andererka <k.anderer@t-online.de>
2022-02-27 18:26:55 +0100

    theta shape

--- code/utils/sbi_modulated_functions.py
+++ code/utils/sbi_modulated_functions.py
@@ -82,6 +82,11 @@ class Combined(Distribution):
                 theta_posterior = torch.unsqueeze(theta_posterior, 0)
                 theta_prior = torch.unsqueeze(theta_prior, 0)
 
+            if theta_prior.dim() == 1:
+                
+                theta_posterior = torch.unsqueeze(theta_posterior, 0)
+                theta_prior = torch.unsqueeze(theta_prior, 0)
+
             print('theta posterior', theta_posterior)
             print('theta prior', theta_prior)
             theta = torch.cat((theta_posterior, theta_prior), 1)
