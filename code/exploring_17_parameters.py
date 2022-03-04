00a715ef99e8ceba60a7dac05c2017615c12e5d4
andererka <k.anderer@t-online.de>
2022-03-04 20:14:13 +0100

    add histogram py

---
+++ code/exploring_17_parameters.py
@@ -0,0 +1,714 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Exploring parameters
+# 
+# #### density plots, post predictive checks etc.
+
+# In[10]:
+
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+import os.path as op
+import tempfile
+
+import matplotlib.pyplot as plt
+
+
+import hnn_core
+from hnn_core import simulate_dipole, jones_2009_model
+from hnn_core.viz import plot_dipole
+
+
+import sys
+sys.path.append('../code/')
+sys.path.append('../code/utils/')
+sys.path.append('../../results_cluster/')
+
+
+import numpy as np
+import torch
+
+# visualization
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+import os
+
+#work_dir = '/home/ubuntu/sbi_for_eeg_data/code/'
+
+#os.chdir(work_dir)
+
+#from utils.plot import cov, compare_vars, plot_varchanges
+#from utils.plot import compare_KLs, plot_KLs
+import utils.sbi_modulated_functions
+
+# sbi
+from sbi import utils as utils
+from sbi import analysis as analysis
+from sbi.inference.base import infer
+from sbi.inference import SNPE_C, prepare_for_sbi, simulate_for_sbi
+
+from sbi.analysis import conditional_pairplot, conditional_corrcoeff
+
+
+
+# import the summary statistics that you want to investigate
+from summary_features.calculate_summary_features import calculate_summary_statistics_alternative as alternative_sumstats
+from summary_features.calculate_summary_features import calculate_summary_stats_temporal as temporal_sumstats
+from summary_features.calculate_summary_features import calculate_summary_stats_number as number_sumstats
+from summary_features.calculate_summary_features import calculate_summary_stats_temporal
+
+
+# In[11]:
+
+
+print(torch.__version__)
+
+
+# In[12]:
+
+
+## defining neuronal network model
+
+from utils.simulation_wrapper import event_seed, set_network_default, SimulationWrapper
+sim_wrapper = SimulationWrapper(25, small_steps=True)
+
+
+# In[13]:
+
+
+window_len = 30
+prior_min = [0, 0, 0, 0, 0, 0, 0, 0, 17.3,    # prox1 weights
+            0, 0, 0, 0, 0, 0, 51.980,            # distal weights
+            0, 0, 0, 0, 0, 0, 0, 0, 112.13]       # prox2 weights
+
+
+
+prior_max = [0.927, 1.0, 0.160, 1.0,  2.093, 1.0, 0.0519, 1.0, 35.9,
+            0.0394, 0.117, 0.000042, 0.025902, 0.854, 0.480, 75.08, 
+            0.000018, 1.0, 8.633, 1.0, 0.05375, 1.0, 4.104,  1.0, 162.110]
+
+true_params = torch.tensor([[0.277, 0.3739, 0.0399, 0.0, 0.6244, 0.3739, 0.034, 0.0, 18.977, 
+                0.011467, 0.06337, 0.000012, 0.013407, 0.466095, 0.0767, 63.08, 
+                0.000005, 0.116706, 4.6729, 0.016733, 0.011468, 0.061556, 2.33, 0.0679, 120.86]])
+
+prior = utils.torchutils.BoxUniform(low=prior_min, high=prior_max)
+
+#number_simulations = 10
+density_estimator = 'nsf'
+
+
+# In[14]:
+
+
+
+#assert (prior.event_shape==torch.Size([25]))
+from utils import inference
+
+
+# In[19]:
+
+
+from utils import inference
+
+
+import pickle
+from data_load_writer import *
+from data_load_writer import load_from_file as lf
+
+import os
+
+#work_dir = '/home/ubuntu/sbi_for_eeg_data/'
+work_dir = '/mnt/qb/work/macke/kanderer29/results/'
+os.chdir(work_dir)
+
+
+
+import os
+
+print(os.getcwd())
+
+#os.chdir('/home/kathi/Documents/Master_thesis/results_cluster/')
+
+
+
+#print(os.getcwd())
+
+#os.chdir('/home/kathi/Documents/Master_thesis/results_cluster')
+
+## loading simulations from previously saved computations
+#file = 'ERP_sequential_3params/step3'
+#file = 'ERP_save_sim_nsf_num_params3'
+#file = 'eval_features'
+file = '10000_multi_round_num_params_25newparams'
+
+#file = '10000_sims_25_fake_obs_3steps'
+
+os.chdir('..')
+print(os.getcwd())
+
+os.chdir('results')
+
+print(os.getcwd())
+  
+
+thetas = torch.load('{}/thetas.pt2.pt'.format(file))
+
+posterior = torch.load('{}/posterior.pt2.pt'.format(file))
+
+x_without = torch.load('{}/obs_without.pt2.pt'.format(file))
+
+x = calculate_summary_stats_temporal(x_without)
+
+
+#true_params = torch.tensor([[0.0274, 19.01, 0.1369, 61.89, 0.1435, 120.86]])
+#true_params = torch.tensor([[  18.9700, 63.5300, 137.1200]])
+#true_params = torch.load('results/{}/true_params.pt'.format(file))
+#true_params = torch.tensor([[0.277, 0.0399, 0.3739, 0.034, 18.977, 0.0115, 0.000012, 0.466, 0.06337, 0.0134, 0.0766, 63.08, 0.000005, 4.6729, 0.0115, 0.3308, 120.86]])
+
+obs_real = torch.load('{}/obs_real.pt'.format(file))
+
+
+os.chdir('')
+
+os.chdir('/sbi_for_eeg_data/code')
+
+
+# ## Inference step:
+
+# In[7]:
+
+
+density_estimator = 'nsf'
+
+
+
+#inf = SNPE(prior=prior, density_estimator = density_estimator)
+
+#inf = SNPE_C(prior, density_estimator="nsf")
+
+#inf = inf.append_simulations(thetas, x)
+
+#density_estimator = inf.train()
+
+#posterior = inf.build_posterior(density_estimator)
+
+
+#true_params = torch.tensor([[26.61, 63.53,  137.12]])
+
+
+# ## Simulation under 'true parameters'
+
+# In[8]:
+
+
+#obs_real = inference.run_only_sim(true_params, simulation_wrapper = sim_wrapper)
+#obs_real = torch.load('{}/obs_real.pt'.format(file))
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+obs_real_stat = calculate_summary_stats_temporal(obs_real)
+
+posterior.set_default_x(obs_real_stat)
+
+
+# In[21]:
+
+
+samples = posterior.sample((100000,), x=obs_real_stat)
+
+
+# In[ ]:
+
+
+parameter_names = ["prox1_ampa_l2_bas","prox1_nmda_l2_bas","prox1_ampa_l2_pyr", "prox1_nmda_l2_pyr", "prox1_ampa_l5_bas", "prox1_nmda_l5_bas", "prox1_ampa_l5_pyr", "prox1_nmda_l5_pyr",
+"t_prox1",
+"dist_ampa_l2_bas", "dist_nmda_l2_bas", "dist_ampa_l2_pyr", "dist_nmda_l2_pyr", "dist_ampa_l5_pyr","dist_nmda_l5_pyr",
+"t_dist", 
+"prox2_ampa_l2_bas","prox2_nmda_l2_bas","prox2_ampa_l2_pyr", "prox2_nmda_l2_pyr", "prox2_ampa_l5_bas", "prox2_nmda_l5_bas", "prox2_ampa_l5_pyr", "prox2_nmda_l5_pyr",
+"t_prox2"]
+
+
+# In[ ]:
+
+
+##better limits:
+
+list_min = torch.min(samples, 0)[0]
+list_max = torch.max(samples, 0)[0]
+
+print(list_min)
+
+print(list_max)
+
+diff = torch.abs(list_max - list_min) * 0.5
+
+print(diff)
+
+list_min = list(list_min - diff)
+list_max = list(list_max + diff)
+
+limits = [list(tup) for tup in zip(list_min, list_max)]
+
+
+# In[13]:
+
+
+#limits = [list(tup) for tup in zip(prior_min, prior_max)]
+
+
+plt.set_cmap('viridis')
+
+fig, axes = analysis.pairplot(
+    samples,
+    limits=limits,
+    upper = 'kde',
+    ticks=np.round(limits,2),
+    figsize=(30, 30),
+    points=true_params,
+    points_offdiag={"markersize": 6},
+    points_colors="r",
+    labels=parameter_names,
+)
+
+for i in range(5):
+    axes[i][i].xaxis.label.set_color('magenta')
+for i in range(5, 12):
+    axes[i][i].xaxis.label.set_color('navy')
+for i in range(12, 17):
+    axes[i][i].xaxis.label.set_color('deeppink')
+    
+    
+plt.savefig('density_plot')
+
+
+# In[14]:
+
+
+posterior.set_default_x(obs_real_stat)
+condition = posterior.sample((1,))
+
+
+# In[15]:
+
+
+_ = analysis.conditional_pairplot(
+    density=posterior,
+    condition=condition,
+    limits=limits,
+    figsize=(20, 20),
+    points=true_params,
+    points_offdiag={"markersize": 6},
+    points_colors="r",
+    labels=parameter_names,
+    #color_map = ['Blues', 'Reds'],
+    #alpha1 = 0.8,
+    #alpha2 = 0.4
+ 
+)
+
+plt.savefig('conditional_density.png')
+
+
+# In[16]:
+
+
+samples = posterior.sample((100,), x=obs_real_stat)
+
+
+# In[ ]:
+
+
+s_x = inference.run_only_sim(samples, simulation_wrapper=sim_wrapper, num_workers=8)
+
+
+# In[ ]:
+
+
+torch.save(s_x, 's_x.pt')
+
+
+# In[ ]:
+
+
+### sample from prior now
+num_samples = 100
+samples_prior = []
+
+
+for i in range(num_samples):
+    sample = prior.sample()
+    samples_prior.append(sample)
+    
+
+
+# In[ ]:
+
+
+samples_prior[:][0].shape
+
+
+# In[ ]:
+
+
+
+s_x_prior = inference.run_only_sim(samples_prior, sim_wrapper, num_workers=8)
+
+torch.save(s_x_prior, 's_x_prior.pt')
+
+
+# In[20]:
+
+
+s_x_torch = torch.stack(([s_x[i] for i in range(len(s_x))]))
+s_x_prior_torch = torch.stack(([s_x_prior[i] for i in range(len(s_x_prior))]))
+
+
+mean = torch.mean(s_x_torch, 0)
+std = torch.std(s_x_torch, 0)
+
+mean_prior = torch.mean(s_x_prior_torch, 0)
+std_prior = torch.std(s_x_prior_torch, 0)
+
+lower = mean - 1.96 * std
+
+
+upper = mean + 1.96 * std
+
+
+lower_prior = mean_prior - 1.96 * std_prior
+
+
+upper_prior = mean_prior + 1.96 * std_prior
+
+
+# In[23]:
+
+
+import seaborn as sns
+
+sns.set() 
+
+sns.set_style("whitegrid", {'axes.grid' : False})
+#sns.set_style('ticks')
+
+fig1, ax = plt.subplots(1, 1)
+#ax.set_title("Comparing signal")
+
+    
+plt.plot(mean, color ='blue', label='mean of posterior')
+
+for s in s_x:
+    plt.plot(s, alpha=0.05, color='blue')
+    #plt.ylim(-30,30)
+    plt.xlim(0, 7000)
+
+plt.plot(lower, color='blue', linestyle='dashed', label='95% confidence')
+plt.plot(upper, color='blue', linestyle='dashed')
+plt.fill_between(x= torch.arange(len(mean_prior)), y1=lower, y2=upper, color='blue', alpha=0.1)
+plt.xlim(0, 7000)
+
+
+plt.plot(mean_prior, color ='orange', label='mean of prior')
+
+
+for x_w in s_x_prior:
+    plt.plot(x_w, alpha=0.05, color='orange')
+
+plt.plot(lower_prior, color='orange', linestyle='dashed', label='95% confidence')
+plt.plot(upper_prior, color='orange', linestyle='dashed')
+plt.fill_between(x= torch.arange(len(mean_prior)), y1=lower_prior, y2=upper_prior, color='orange', alpha=0.2)
+plt.xlim(0, 7000)
+
+plt.xlabel('time in ms')
+#plt.ylabel('voltage ()')
+
+fig1.gca().set_ylabel(r'voltage ($\mu V$)')
+    
+plt.plot(obs_real[0], label='Ground truth', color='red')
+
+
+
+plt.legend()
+
+plt.savefig('posterior_predictive.png')
+
+
+# ## Correlation matrices
+
+# In[24]:
+
+
+corr_matrix_marginal = np.corrcoef(posterior_samples.T)
+fig, ax = plt.subplots(1,1, figsize=(4, 4))
+im = plt.imshow(corr_matrix_marginal, clim=[-1, 1], cmap='PiYG')
+_ = fig.colorbar(im)
+
+
+# In[ ]:
+
+
+condition = posterior.sample((1,))
+
+_ = conditional_pairplot(
+    density=posterior,
+    condition=condition,
+    limits=torch.tensor([[-2., 2.]]*3),
+    figsize=(5,5)
+)
+
+plt.savefig('correlation_matrix.png')
+
+
+# ### Histogram plots
+
+# In[ ]:
+
+
+s_x_prior_stat = prior.sample((1000,))
+
+s_x_stat = posterior.sample((1000,))
+
+
+# In[ ]:
+
+
+import matplotlib.gridspec as gridspec
+
+sum_stats_names =                  [
+                    'arg_p50',
+                    'arg_N100',
+                    'arg_P200',
+                    'p50',
+                    'N100',
+                    'P200',
+                    'p50_moment1',
+                    'N100_moment1',
+                    'P200_moment1',
+                    'p50_moment2',
+                    'N100_moment2',
+                    'P200_moment2',
+                    'area_pos1',
+                    'area_neg1',
+                    'area_pos2',
+                    'area_neg2',
+                    'area_pos3',
+                    'area_neg3',
+                    'mean4000',
+                    'mean1000'
+                ]
+
+fig = plt.figure(figsize=(10,5*s_x_prior_stat.shape[1]), tight_layout=True)
+
+gs = gridspec.GridSpec(nrows=len(sum_stats_names), ncols=1)
+
+
+
+#fig.suptitle('Summary stats histogram from posterior predictions.', y=0.2, fontsize=16)
+
+
+for i in range(len(sum_stats_names)):
+
+    globals()['ax%s' % i] = fig.add_subplot(gs[i])
+
+    globals()['sum_stats%s' % i] = []
+    globals()['x%s' % i] = []
+
+    for j in range(s_x_prior_stat.shape[0]):
+        globals()['sum_stats%s' % i].append(s_x_stat[j][i])
+        globals()['x%s' % i].append(s_x_prior_stat[j][i])
+
+    sum_stat = globals()['sum_stats%s' % i]
+    
+    
+    ##define bins such that we get an equal number of bins at the end
+    
+    binsteps = np.abs(max(sum_stat)-min(sum_stat))*0.03 + 0.001
+    
+   
+    binrange = np.arange(min(sum_stat), max(sum_stat) + binsteps, binsteps)
+    
+   
+    
+    globals()['ax%s' % i].hist(globals()['sum_stats%s' % i],  density=True, bins = binrange,  facecolor='g', alpha=0.75, histtype='barstacked', label='from posterior')
+    globals()['ax%s' % i].hist(globals()['x%s' % i],  density=True, bins = binrange,   facecolor='b', alpha=0.5, histtype='barstacked', label='from proposal/prior')
+    
+  
+    globals()['ax%s' % i].set_title('Histogram of summary stat "{}" '.format(sum_stats_names[i]), pad=20)
+    #ax0.set(ylim=(-500, 7000))
+
+    globals()['ax%s' % i].axvline(obs_real_stat[0][i].detach().numpy(), color='red', label='ground truth')
+    globals()['ax%s' % i].legend(loc='upper right')
+    
+    
+plt.savefig('histogram.png')
+
+
+# ## Evaluation of features
+
+# In[28]:
+
+
+import matplotlib.cm as cm
+import re
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from torch import Tensor
+from typing import Tuple, List, Optional, Dict
+from matplotlib.pyplot import Axes
+from matplotlib.figure import Figure
+from matplotlib.colors import LogNorm
+
+
+# In[68]:
+
+
+t = obs_real_stat
+
+t
+
+
+# In[74]:
+
+
+# t.shape
+
+t1 = torch.cat((t[:,:1], t[:,1+1:]), axis = 1)
+print(t1)
+t1.shape
+
+
+# In[54]:
+
+
+posterior.set_default_x(obs_real_stat)
+
+
+# In[55]:
+
+
+thetas = posterior.sample((10000,))
+thetas.shape
+
+
+# In[79]:
+
+
+
+for i in range(20):
+    x = calculate_summary_stats_temporal(x_without)
+    #leaving one summary statistic out
+    x_c = torch.cat((x[:,:i], x[:,i+1:]), axis = 1)
+    print(x_c.shape)
+    print('t shape', t.shape)
+    print(torch.cat((t[:i], t[i+1:]), axis = 0).shape)
+    inf = SNPE(prior=prior, density_estimator = 'nsf')
+    inf = inf.append_simulations(thetas, x_c)
+    density_estimator = inf.train()
+    posterior1 = inf.build_posterior(density_estimator)
+    globals()['samples_%s' % i] = posterior1.sample((num_samples,), x=torch.cat((t[:,:i], t[:,i+1:]), axis = 1))
+
+
+# In[106]:
+
+
+sum_stats_names =                 [
+                    'arg_p50',
+                    'arg_N100',
+                    'arg_P200',
+                    'p50',
+                    'N100',
+                    'P200',
+                    'p50_mo1',
+                    'N100_mo1',
+                    'P200_mo1',
+                    'p50_mo2',
+                    'N100_mo2',
+                    'P200_mo2',
+                    'area_pos1',
+                    'area_neg1',
+                    'area_pos2',
+                    'area_neg2',
+                    'area_pos3',
+                    'area_neg3',
+                    'mean4000',
+                    'mean1000'
+                ]
+
+sample_list = []
+for i in range(20):
+    sample_list.append(globals()['samples_%s' % i] )
+
+    
+plt.figure(figsize = (25,20))
+im = plot_varchanges(sample_list, samples, xticklabels=sum_stats_names, yticklabels= parameter_names, plot_label='', batchsize=0)
+plt.show(im)
+
+
+# In[120]:
+
+
+sample_batch = []
+batch_size = 5
+
+for i in range(batch_size):
+
+    sample_list = []
+    
+    for i in range(20):
+        
+
+        x = calculate_summary_stats_temporal(x_without)
+        #leaving one summary statistic out
+        x_c = torch.cat((x[:,:i], x[:,i+1:]), axis = 1)
+        print(x_c.shape)
+        print('t shape', t.shape)
+        print(torch.cat((t[:i], t[i+1:]), axis = 0).shape)
+        inf = SNPE(prior=prior, density_estimator = 'nsf')
+        inf = inf.append_simulations(thetas, x_c)
+        density_estimator = inf.train()
+        posterior1 = inf.build_posterior(density_estimator)
+        globals()['samples_%s' % i] = posterior1.sample((num_samples,), x=torch.cat((t[:,:i], t[:,i+1:]), axis = 1))
+        
+        sample_list.append(globals()['samples_%s' % i] )
+        
+    sample_batch.append(sample_list)
+
+
+# In[119]:
+
+
+sample_batch
+
+
+# In[117]:
+
+
+fig, axes = plt.subplots(1, 1, figsize=(10, 10), sharex=True)
+
+
+plot_KLs(sample_list,
+        samples,
+        idx=0,
+        batchsize=0,
+         kind='bar',
+        agg_with='mean'
+    )
+
+
+axes.set_xlabel("missing feature", size=14)
+axes.set_xticklabels(sum_stats_names)
+ax.tick_params(axis="both", which="major", labelsize=12)
+ylabel = ax.get_ylabel()
+axes.set_ylabel(ylabel, size=14)
+
