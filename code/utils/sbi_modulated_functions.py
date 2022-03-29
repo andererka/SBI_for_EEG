28eabf84d8d988aa4106658cf3d0650d4689cc12
andererka <k.anderer@t-online.de>
2022-03-29 16:12:19 +0200

    Combined class2

--- code/utils/sbi_modulated_functions.py
+++ code/utils/sbi_modulated_functions.py
@@ -191,5 +191,97 @@ class Combined(Distribution):
 
 
 
+class Combined1(Distribution):
+    """
+    Inherits from Torch Distribution class
+    implements own log_prob() and sample() for two different prior distributions such that parameter sets can be inferred sequentially - one posterior is based on another
+   
+    takes as arguments:
+    - posterior distribution of already inferred parameter set
+    - prior distribution of subsequent parameter set that is dependent on earlier parameter set
+    - number_params_1: number of parameters that was inferred already (in posterior distribution)
+    """
+
+    has_rsample = False
+    support = constraints.real
+
+    def __init__(
+        self,
+        posterior_distribution,
+        prior_distribution,
+        validate_args=None,
+        batch_shape=torch.Size(),
+        event_shape=torch.Size(),
+        number_params_1=0,
+    ):
+        self._batch_shape = batch_shape
+        self._event_shape = event_shape
+        self._posterior_distribution = posterior_distribution
+        self._prior_distribution = prior_distribution
+        self.number_params = number_params_1
+
+        super(Combined, self).__init__(batch_shape, validate_args=validate_args)
+
+    def log_prob(self, x):
+
+        """
+        calculates the log probability of the combined prior distribution based on some observation x
+        """
+        index = self.number_params
+
+        log_prob_posterior = self._posterior_distribution.log_prob(x[0][:index])
+        log_prob_prior = self._prior_distribution.log_prob(x[0][index:])
+
+        log_prob = torch.add(log_prob_posterior, log_prob_prior)
+
+        print(log_prob)
+
+
+        return log_prob
+
+    def sample(self, sample_shape=torch.Size()):
+
+        """
+        samples from combined prior distribution
+        """
+
+        with torch.no_grad():
+
+            theta_posterior = self._posterior_distribution.sample(sample_shape)
+            theta_prior = self._prior_distribution.sample(sample_shape)
+    
+            #make sure that thetas are in the right shape; otherwise unsqueeze:
+            if theta_posterior.dim() == 1:
+                
+                theta_posterior = torch.unsqueeze(theta_posterior, 0)
+
+            if theta_prior.dim() == 1:
+
+                theta_prior = torch.unsqueeze(theta_prior, 0)
+
+
+            theta = torch.cat((theta_posterior, theta_prior), 1)
+
+
+        
+            return theta
+
+
+    @property
+    def mean(self):
+        """
+        Returns the mean of the distribution.
+        """
+        return torch.mean(self.sample((1000,)),dim = 0)
+
+    @property
+    def variance(self):
+        """
+        Returns the mean of the distribution.
+        """
+        return torch.var(self.sample((1000,)),dim = 0)
+
+
+
 
 
