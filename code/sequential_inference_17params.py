865e2b8948594841b59ca2bb4920157a3d9e0c22
andererka <k.anderer@t-online.de>
2022-04-07 13:36:14 +0200

    ratio sequential

--- code/sequential_inference_17params.py
+++ code/sequential_inference_17params.py
@@ -194,8 +194,8 @@ def main(argv):
         theta, x_without = inference.run_sim_theta_x(
             prior1, 
             sim_wrapper,
-            #num_simulations=int(num_sim*(1/10)),
-            num_simulations = num_sim,
+            num_simulations=int(num_sim*(1/10)),
+            #num_simulations = num_sim,
             num_workers=num_workers
         )
 
@@ -332,8 +332,8 @@ def main(argv):
         theta, x_without = inference.run_sim_theta_x(
             combined_prior,
             sim_wrapper,
-            #num_simulations=int(num_sim*(19/10)),
-            num_simulations = num_sim,
+            num_simulations=int(num_sim*(19/10)),
+            #num_simulations = num_sim,
             num_workers = num_workers
         )
 
