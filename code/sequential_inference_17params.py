1490245d36e2704216e23ae4f72c91b42a22495b
andererka <k.anderer@t-online.de>
2022-04-28 12:21:34 +0200

    1/30

--- code/sequential_inference_17params.py
+++ code/sequential_inference_17params.py
@@ -213,7 +213,7 @@ def main(argv):
         theta, x_without = inference.run_sim_theta_x(
             prior1, 
             sim_wrapper,
-            num_simulations=int(num_sim*(1/10)),
+            num_simulations=int(num_sim*(1/30)),
             #num_simulations = num_sim,
             num_workers=num_workers
         )
@@ -369,7 +369,7 @@ def main(argv):
         theta, x_without = inference.run_sim_theta_x(
             combined_prior,
             sim_wrapper,
-            num_simulations=int(num_sim*(19/10)),
+            num_simulations=int(num_sim*(59/30)),
             #num_simulations = num_sim,
             num_workers = num_workers
         )
