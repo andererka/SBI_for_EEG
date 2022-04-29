b09010531dad6216df673949508c446495107732
andererka <k.anderer@t-online.de>
2022-04-29 15:39:11 +0200

    other approach to prevent overdispersion

--- code/toy_example_gaussians.py
+++ code/toy_example_gaussians.py
@@ -128,8 +128,8 @@ def main(argv):
 
     # In[6]:
 
-    num_simulations_list = [750]
-    #num_simulations_list = [1000, 1500, 2000, 3000]
+    #num_simulations_list = [750]
+    num_simulations_list = [500, 750, 1000, 1500, 2000, 3000]
     #num_simulations_list = [100]
 
 
